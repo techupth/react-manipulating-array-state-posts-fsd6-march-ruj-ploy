26653ddd1e4948ec93cf649602bda3d4ded63419
nattawara <nattawara.jtk@gmail.com>
2024-06-06 15:18:31 +0700

    feat:add likeCount to posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,29 +1,49 @@
+import data from "./data";
+import { useState } from "react";
 function Posts() {
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
-            </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
-      </div>
+      <ul>
+        {data.map((item) => {
+          const [likeCount, setLikeCount] = useState(0);
+          let likeTotal = likeCount + item.likes;
+          const handleLike = () => {
+            setLikeCount(likeCount + 1);
+          };
+          const handleDislike = () => {
+            if (likeTotal > 0) {
+              setLikeCount(likeCount - 1);
+            }
+          };
+          return (
+            <>
+              <div class="post-list">
+                <div class="post-item">
+                  <div class="post-header">
+                    <h2>
+                      {item.title} #{item.id}
+                    </h2>
+                    <div class="post-social-media-stats">
+                      <span class="stats-topic">Likes: </span>
+                      <span class="post-likes">{likeTotal}</span>
+                    </div>
+                  </div>
+                  <p class="post-content">{item.content}</p>
+                  <div class="post-actions">
+                    <button class="like-button" onClick={handleLike}>
+                      Like
+                    </button>
+                    <button class="dislike-button" onClick={handleDislike}>
+                      Dislike
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </ul>
     </div>
   );
 }
