8fb0663b3282423d5341254da6cca39acb9905be
nattawara <nattawara.jtk@gmail.com>
2024-06-07 18:07:28 +0700

    fix: refactor

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,21 +1,31 @@
 import data from "./data";
 import { useState } from "react";
 function Posts() {
+  const [likesCount, setLikesCount] = useState([]);
+  const handleLike = (index) => {
+    const newLikesCount = [...likesCount];
+    if (newLikesCount[index] === undefined) {
+      newLikesCount[index] = data[index].likes + 1;
+    } else {
+      newLikesCount[index] += 1;
+    }
+    setLikesCount(newLikesCount);
+  };
+  const handleDislike = (index) => {
+    const newLikesCount = [...likesCount];
+    if (newLikesCount[index] === undefined && data[index].likes > 0) {
+      newLikesCount[index] = data[index].likes - 1;
+    } else if (newLikesCount[index] > 0) {
+      newLikesCount[index] -= 1;
+    }
+
+    setLikesCount(newLikesCount);
+  };
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <ul>
         {data.map((item, index) => {
-          const [likeCount, setLikeCount] = useState(0);
-          const likeTotal = likeCount + item.likes;
-          const handleLike = () => {
-            setLikeCount(likeCount + 1);
-          };
-          const handleDislike = () => {
-            if (likeTotal > 0) {
-              setLikeCount(likeCount - 1);
-            }
-          };
           return (
             <>
               <div class="post-list">
@@ -26,15 +36,27 @@ function Posts() {
                     </h2>
                     <div class="post-social-media-stats">
                       <span class="stats-topic">Likes: </span>
-                      <span class="post-likes">{likeTotal}</span>
+                      <span class="post-likes">
+                        {likesCount[index] === undefined
+                          ? item.likes
+                          : likesCount[index]}
+                      </span>
                     </div>
                   </div>
                   <p class="post-content">{item.content}</p>
                   <div class="post-actions">
-                    <button class="like-button" onClick={handleLike}>
+                    <button
+                      class="like-button"
+                      onClick={() => handleLike(index)}
+                    >
                       Like
                     </button>
-                    <button class="dislike-button" onClick={handleDislike}>
+                    <button
+                      class="dislike-button"
+                      onClick={() => {
+                        handleDislike(index);
+                      }}
+                    >
                       Dislike
                     </button>
                   </div>
